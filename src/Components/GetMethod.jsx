7810942213bd40b14d7abc32a3d3f11dc7290a82
CleverCoder03 <clevercoder0307@gmail.com>
2022-02-21 11:43:53 +0530

    Updated Get method component

--- src/Components/GetMethod.jsx
+++ src/Components/GetMethod.jsx
@@ -19,46 +19,77 @@ function GetMethod() {
 
   return (
     <div className="Get_Method">
+      <h1 class="h1 text-primary text-center font-weight-bold">GET Method</h1>
       <div id="navbar" className="container-sm">
         <div>
-        <button type="button" class="btn btn-success m-3" onClick={() => { setShow(true); }}>Show Data</button>
-        <button type="button" class="btn btn-danger m-3" onClick={() => { setShow(false); }}>Clear Data</button>
+          <button
+            type="button"
+            class="btn btn-success m-3"
+            onClick={() => {
+              setShow(true);
+            }}
+          >
+            Show Data
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger m-3"
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            Clear Data
+          </button>
         </div>
         <div>
-        <form class="d-flex">
-          <input class="form-control me-2" type="text/number" placeholder="Search by ID, Title" aria-label="Search" onChange={(e)=>{ setSearchTerm(e.target.value) }} />
-      </form>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="text/number"
+              placeholder="Search by ID, Title"
+              aria-label="Search"
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
+          </form>
         </div>
       </div>
       <div className="container">
-        {show
-          ? user.filter((userData)=>{
-            if(searchTerm === ""){
-              return userData;
-            } else if ( userData.title.toLowerCase().includes(searchTerm.toLowerCase())){
-              return userData;
-            }
-          }).map((userData) => (
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Page Count</th>
-                  </tr>
-                </thead>
-
-                <tbody className="animationOnLoad">
-                  <tr key={userData.id}>
-                    <td>{userData.id}</td>
-                    <td>{userData.title}</td>
-                    <td>{userData.description}</td>
-                    <td>{userData.pageCount}</td>
-                  </tr>
-                </tbody>
-              </table>
-            ))
+      {show
+          ? user
+              .filter((userData) => {
+                if (searchTerm === "") {
+                  return userData;
+                } else if (
+                  userData.title
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+                ) {
+                  return userData;
+                }
+              })
+              .map((userData) => (
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Id</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Page Count</th>
+                    </tr>
+                  </thead>
+                  
+                  <tbody className="animationOnLoad">
+                    <tr key={userData.id}>
+                      <td>{userData.id}</td>
+                      <td>{userData.title}</td>
+                      <td>{userData.description}</td>
+                      <td>{userData.pageCount}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              ))
           : null}
       </div>
     </div>
